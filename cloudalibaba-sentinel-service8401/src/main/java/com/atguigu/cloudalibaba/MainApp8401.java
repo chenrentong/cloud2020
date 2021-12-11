ed0cfefc7744139e255bfc1d81b333f97deb5910
chenrentong <846722718@qq.com>
2021-12-12 00:06:32 +0800

    sentinel

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloudalibaba/MainApp8401.java
@@ -0,0 +1,19 @@
+package com.atguigu.cloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author chenrentong
+ * @create 2021-12-11 22:52
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MainApp8401
+{
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+}
+
